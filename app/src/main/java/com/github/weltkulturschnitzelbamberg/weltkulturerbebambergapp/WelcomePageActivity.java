8bb68b431b8db7fd361922d7c25a491cdc656a91
OrangeUtan <Oran9eUtan@gmail.com>
2015-06-04 21:47:26 +0200

    Documentation für die Activity 'WelcomePageActivity' hinzugefügt

--- app/src/main/java/com/github/weltkulturschnitzelbamberg/weltkulturerbebambergapp/WelcomePageActivity.java
+++ app/src/main/java/com/github/weltkulturschnitzelbamberg/weltkulturerbebambergapp/WelcomePageActivity.java
@@ -6,7 +6,13 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-
+/**
+ * Diese Activity ist die StartActivity der Weltkulturerbe-Application
+ *
+ * @author Projekt-Seminar "Schnitzeljagd World-heritage" 2015/2016 des Clavius Gymnasiums Bamberg
+ * @version 1.0
+ * @since 2015-06-04
+ */
 public class WelcomePageActivity extends Activity {
 
     @Override
