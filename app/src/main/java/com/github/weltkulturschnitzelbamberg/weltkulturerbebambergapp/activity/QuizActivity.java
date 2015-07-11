5679b33d60521a090364a3d5a9567b91462845fa
OrangeUtan <Oran9eUtan@gmail.com>
2015-07-11 18:13:12 +0200

    "QuizActivity" hinzugefügt. Diese Activity soll die verschiedenen Quiz anzeigen

---
+++ app/src/main/java/com/github/weltkulturschnitzelbamberg/weltkulturerbebambergapp/activity/QuizActivity.java
@@ -0,0 +1,18 @@
+package com.github.weltkulturschnitzelbamberg.weltkulturerbebambergapp.activity;
+
+import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.github.weltkulturschnitzelbamberg.weltkulturerbebambergapp.R;
+
+public class QuizActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+    }
+}
