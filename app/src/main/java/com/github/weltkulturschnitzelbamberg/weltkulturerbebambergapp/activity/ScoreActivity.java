cdf149c6bd1cd2ecad8be20c501727118354459e
OrangeUtan <Oran9eUtan@gmail.com>
2015-07-14 19:45:51 +0200

    "ScoreActivity" an die neue ActionBar und das neue Button-Design angepasst

--- app/src/main/java/com/github/weltkulturschnitzelbamberg/weltkulturerbebambergapp/activity/ScoreActivity.java
+++ app/src/main/java/com/github/weltkulturschnitzelbamberg/weltkulturerbebambergapp/activity/ScoreActivity.java
@@ -19,27 +19,11 @@ import com.github.weltkulturschnitzelbamberg.weltkulturerbebambergapp.R;
  * @version 1.0
  * @since 2015-06-04
  */
-public class ScoreActivity extends Activity implements AppCompatCallback {
-
-    private AppCompatDelegate delegate;
-
+public class ScoreActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        //TODO cohesion
-        //let's create the delegate, passing the activity at both arguments (Activity, AppCompatCallback)
-        delegate = AppCompatDelegate.create(this, this);
-        //we need to call the onCreate() of the AppCompatDelegate
-        delegate.onCreate(savedInstanceState);
-
-        //we use the delegate to inflate the layout
-        delegate.setContentView(R.layout.activity_score);
-
-        //Finally, let's add the Toolbar
-        Toolbar toolbar = (Toolbar) findViewById(R.id.score_toolbar);
-        delegate.setSupportActionBar(toolbar);
 
         //TODO Remvove onClickListener
         Button btn_score_back_to_start = (Button) findViewById(R.id.btn_score_back_to_start);
@@ -48,15 +32,5 @@ public class ScoreActivity extends Activity implements AppCompatCallback {
                 startActivity(new Intent(ScoreActivity.this, WelcomePageActivity.class));
             }
         });
-    }
-
-    @Override
-    public void onSupportActionModeStarted(ActionMode mode) {
-        //let's leave this empty, for now
-    }
-
-    @Override
-    public void onSupportActionModeFinished(ActionMode mode) {
-        // let's leave this empty, for now
     }
 }
