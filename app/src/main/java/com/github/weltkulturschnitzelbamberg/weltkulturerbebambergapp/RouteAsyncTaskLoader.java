1b3598b77f2f1c6a2e38268d63871b5bfb536307
OrangeUtan <Oran9eUtan@gmail.com>
2015-07-23 14:39:56 +0200

    "RouteAsyncTaskLoader" lädt "routes.json"

--- app/src/main/java/com/github/weltkulturschnitzelbamberg/weltkulturerbebambergapp/RouteAsyncTaskLoader.java
+++ app/src/main/java/com/github/weltkulturschnitzelbamberg/weltkulturerbebambergapp/RouteAsyncTaskLoader.java
@@ -8,6 +8,10 @@ import com.github.weltkulturschnitzelbamberg.weltkulturerbebambergapp.contentpro
 import com.github.weltkulturschnitzelbamberg.weltkulturerbebambergapp.database.RoutesTable;
 import com.github.weltkulturschnitzelbamberg.weltkulturerbebambergapp.utilities.DebugUtils;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -16,6 +20,7 @@ import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -52,77 +57,38 @@ public class RouteAsyncTaskLoader extends AsyncTaskLoader {
     }
 
     // TODO Documentation
-    private void writeRoutesToDatabase() throws IOException, ParserConfigurationException, SAXException {
-        Document doc = loadFile();
+    private void writeRoutesToDatabase() throws IOException, ParseException {
+        JSONParser parser = new JSONParser();
+        Object obj = parser.parse(new InputStreamReader(getContext().getAssets().open("routes.json"), "UTF-8"));
 
-        // Get a List of all Routes within the XML Document 'doc'
-        NodeList routes = doc.getElementsByTagName(Routes.TAG_ROUTE);
-        for (int i = 0; i < routes.getLength(); i++) {
-            // If t?he Route in position 'i' in the List of all Routes 'routes' is a Element Node, continue
-            if (routes.item(i).getNodeType() == Node.ELEMENT_NODE) {
-                // 'route' is the Route in position 'i' in the List of all Routes 'routes'
-                Element route = (Element) routes.item(i);
+        if (obj instanceof JSONObject) {
+            JSONObject jsonObject = (JSONObject) obj;
 
-                // Name of the current Route
-                String routeName = null;
-                // If the current Route has a "Name" Tag, 'routeName' is set to its value
-                if (route.getElementsByTagName(Routes.TAG_NAME).getLength() > 0) routeName = route.getElementsByTagName(Routes.TAG_NAME).item(0).getTextContent();
+            JSONArray routes = (JSONArray) jsonObject.get("routes");
+            for (int i = 0; i < routes.size(); i++) {
+                JSONObject route = (JSONObject) routes.get(i);
 
-                // Get a List of all Waypoints within the current Route 'route'
-                NodeList waypoints = route.getElementsByTagName(Routes.TAG_WAYPOINT);
-                for (int k = 0; k < waypoints.getLength(); k++) {
-                    // If the Waypoint in position 'k' in the List of all Waypoints 'waypoints' within the current Route 'route' is a Element Node, continue
-                    if (waypoints.item(k).getNodeType() == Node.ELEMENT_NODE) {
-                        // 'waypoint' is the Waypoint in position 'k' in the List of all Waypoints 'waypoints' within the current Route 'route'
-                        Element waypoint = (Element) waypoints.item(k);
+                String name = null;
+                if (route.get("name") instanceof String) name = (String) route.get("name");
 
-                        // Position of the current Waypoint in the current Route
-                        String position = null;
-                        // ID of the Waypoint in this position
-                        String waypointID = null;
+                JSONArray waypoints = (JSONArray) route.get("waypoints");
+                for (int k = 0; k < waypoints.size(); k++) {
+                    JSONObject waypoint = (JSONObject) waypoints.get(k);
 
-                        // If the current Waypoint has a "Position" Tag, 'position' is set to its value
-                        if (waypoint.getElementsByTagName(Routes.TAG_POSITION).getLength() > 0)
-                            position = waypoint.getElementsByTagName(Routes.TAG_POSITION).item(0).getTextContent();
-                        // If the current Waypoint has a "WaypointID" Tag, 'position' is set to its value
-                        if (waypoint.getElementsByTagName(Routes.TAG_WAYPOINT_ID).getLength() >0)
-                            waypointID = waypoint.getElementsByTagName(Routes.TAG_WAYPOINT_ID).item(0).getTextContent();
+                    Long waypointId = 0L;
+                    Long position = 0L;
 
-                        // Content values containing the individual values of the current waypoint
-                        ContentValues values = new ContentValues();
-                        // 'routeName' will be inserted into the "RouteName" Column
-                        values.put(RoutesTable.COLUMN_ROUTE_NAME, routeName);
-                        // 'position' will be inserted into the "WaypointPosition" Column
-                        values.put(RoutesTable.COLUMN_WAYPOINT_POSITION, position);
-                        // 'waypointID' will be inserted into the "WaypointID" Column
-                        values.put(RoutesTable.COLUMN_WAYPOINT_ID, waypointID);
+                    if (waypoint.get("id") instanceof Long) waypointId = (long) waypoint.get("id");
+                    if (waypoint.get("position") instanceof Long) position = (long) waypoint.get("position");
 
-                        // Instert new Waypoint into the Routes Table in the SQL Database
-                        getContext().getContentResolver().insert(WeltkulturerbeContentProvider.URI_TABLE_ROUTES, values);
-                    }
+                    ContentValues values = new ContentValues();
+                    values.put(RoutesTable.COLUMN_ROUTE_NAME, name);
+                    values.put(RoutesTable.COLUMN_WAYPOINT_ID, waypointId);
+                    values.put(RoutesTable.COLUMN_WAYPOINT_POSITION, position);
+
+                    getContext().getContentResolver().insert(WeltkulturerbeContentProvider.URI_TABLE_ROUTES, values);
                 }
             }
         }
-    }
-
-    // TODO Documentation
-    private Document loadFile() throws IOException, ParserConfigurationException, SAXException{
-        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder docBuilder = dbFactory.newDocumentBuilder();
-        InputSource inputSource = new InputSource(getContext().getAssets().open(Routes.FILENAME));
-        Document doc = docBuilder.parse(inputSource);
-        doc.getDocumentElement().normalize();
-        return doc;
-    }
-
-    //TODO Documentation
-    private static class Routes {
-
-        private static final String FILENAME = "routes.xml";
-        private static final String TAG_ROUTE = "route";
-        private static final String TAG_NAME = "name";
-        private static final String TAG_WAYPOINT = "waypoint";
-        private static final String TAG_POSITION = "position";
-        private static final String TAG_WAYPOINT_ID = "id";
     }
 }
